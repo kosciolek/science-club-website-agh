0a75bc9950abc5ffde5c41b926f3e930d8f43851
kosciolek <45483493+kosciolek@users.noreply.github.com>
2024-12-26 16:34:46 +0100

    init

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import icon from "astro-icon";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    icon(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  // for github pages
+  site: "https://kosciolek.github.io",
+  base: "science-club-website-agh",
+});
---
+++ src/header/const.ts
@@ -0,0 +1,1 @@
+export const MOBILE_MENU_DIALOG_ID = "mobile-menu-dialog";
